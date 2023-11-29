42253115ce491102a573591b642c1639be840735
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2023-11-28 22:02:38 -0300

    Update loginModule.js
    adição do response

--- modules/loginModule.js
+++ modules/loginModule.js
@@ -3,9 +3,6 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 const db = require('../db');
 
-// Substitua as informações do banco de dados com as fornecidas pelo ElephantSQL
-// const db = pgp('postgres://yixmxjjh:146A5tfdyeKyKtGjbBUPxh7ujOaWZFh3@isabelle.db.elephantsql.com/yixmxjjh');
-
 router.post('/', async (req, res) => {
   const { email, senha } = req.body;
 
@@ -14,12 +11,19 @@ router.post('/', async (req, res) => {
   }
 
   try {
-    // Buscar usuário com o e-mail fornecido
     const user = await db.oneOrNone('SELECT * FROM usuarios WHERE email = $1', email);
 
-    // Verificar se o usuário existe e a senha está correta
     if (user && await bcrypt.compare(senha, user.senha)) {
-      res.status(200).json({ message: 'Login bem-sucedido' });
+      const userData = {
+        id: user.id,
+        nome: user.nome,
+        email: user.email,
+      };
+
+      res.status(200).json({
+        message: 'Login bem-sucedido',
+        user: userData,
+      });
     } else {
       res.status(401).json({ error: 'Credenciais inválidas' });
     }
