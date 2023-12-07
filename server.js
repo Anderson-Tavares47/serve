547291198521bbb6de752384d798a455f92c1bf1
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2023-12-07 16:06:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const login = require("./modules/loginModule");
 const usuarios = require("./modules/usuariosModule");
 const UserAssas = require("./modules/usuarioAssasModule");
 const FaturaAssas = require("./modules/FaturasAssasModule");
+const criaPastas = require('./modules/criaPastas');
+const getPastas = require('./modules/pastasGetModule');
+const campanhas = require('./modules/campanhasModule');
+const getCampanhas = require('./modules/campanhasGetModule');
+const putPastas = require('./modules/pastasPutModule');
+const deletePastas = require('./modules/pastasDeleteModule');
+const putCamapanhas = require('./modules/campanhasPutModule');
+const deleteCamapanhas = require('./modules/campanhasDeleteModule');
+const admin = require('./modules/admModule');
 
 const app = express();
 const port = 5000;
@@ -19,6 +28,15 @@ app.use("/login", login);
 app.use("/usuarios", usuarios);
 app.use("/assasUser", UserAssas);
 app.use("/assasFatura", FaturaAssas);
+app.use('/api/pastas', criaPastas);
+app.use('/pastas', getPastas);
+app.use('/api/campanhas', campanhas);
+app.use('/campanhas', getCampanhas);
+app.use('/editarPasta', putPastas);
+app.use('/deletePasta', deletePastas);
+app.use('/editarCampanhas', putCamapanhas);
+app.use('/deleteCampanhas', deleteCamapanhas);
+app.use('/admin', admin);
 
 app.listen(port, () => {
   console.log(`Servidor está ouvindo em http://localhost:${port}`);
