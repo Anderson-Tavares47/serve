7dc3a557f1e722597c1b76813dec00437499e48f
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-02 18:38:15 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,52 @@
+Response {
+  [Symbol(realm)]: { settingsObject: {} },
+  [Symbol(state)]: {
+    aborted: false,
+    rangeRequested: false,
+    timingAllowPassed: false,
+    requestIncludesCredentials: false,
+    type: 'default',
+    status: 200,
+    timingInfo: null,
+    cacheState: '',
+    statusText: '',
+    headersList: HeadersList {
+      cookies: [Array],
+      [Symbol(headers map)]: [Map],
+      [Symbol(headers map sorted)]: null
+    },
+    urlList: [],
+    body: { stream: undefined, source: [Uint8Array], length: 856 }
+  },
+  [Symbol(headers)]: HeadersList {
+    cookies: [
+      'accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1BQTAxNjMwMDAxIiwiY2lmIjoiTUFBMDE2MyIsImZpcnN0TmFtZSI6IkdyZWdvcnkiLCJsYXN0TmFtZSI6IlRlc3QiLCJzZXNzaW9uIjp7InNpZ25hdHVyZSI6ImNkODAyOTJkMzA3MmUzZTNjZjYxMjc1YzdjNjA5MjU0NDhkMjZjNDQwZmNmNGVhNTYwNTI0MjE3NWQ3MjA4YzciLCJ1c2VyQ29udGV4dCI6eyJ1c2VySWQiOiJNQUEwMTYzMDAwMSIsImNpZiI6Ik1BQTAxNjMiLCJmaXJzdE5hbWUiOiJHcmVnb3J5IiwibGFzdE5hbWUiOiJUZXN0In0sInJhdyI6ImV5SnphV2R1WVhSMWNtVWlPaUpqWkRnd01qa3laRE13TnpKbE0yVXpZMlkyTVRJM05XTTNZell3T1RJMU5EUTRaREkyWXpRME1HWmpaalJsWVRVMk1EVXlOREl4TnpWa056SXdPR00zSWl3aWRYTmxja052Ym5SbGVIUWlPbnNpZFhObGNrbGtJam9pVFVGQk1ERTJNekF3TURFaUxDSmphV1lpT2lKTlFVRXdNVFl6SWl3aVptbHljM1JPWVcxbElqb2lSM0psWjI5eWVTSXNJbXhoYzNST1lXMWxJam9pVkdWemRDSjlmUT09In0sImlhdCI6MTcwNjg5OTE0MSwiZXhwIjoxNzA2OTAyNzQxfQ.sSKqnNYDQ1wK6yAylp9Wax9WYt5RPmW-DqI 
+BuZrMnw; HttpOnly'
+    ],
+    [Symbol(headers map)]: Map(22) {
+      'access-control-allow-origin' => [Object],
+      'content-length' => [Object],
+      'content-type' => [Object],
+      'cross-origin-opener-policy' => [Object],
+      'cross-origin-resource-policy' => [Object],
+      'date' => [Object],
+      'origin-agent-cluster' => [Object],
+      'referrer-policy' => [Object],
+      'set-cookie' => [Object],
+      'strict-transport-security' => [Object],
+      'vary' => [Object],
+      'via' => [Object],
+      'x-content-type-options' => [Object],
+      'x-correlation-id' => [Object],
+      'x-dns-prefetch-control' => [Object],
+      'x-download-options' => [Object],
+      'x-frame-options' => [Object],
+      'x-kong-proxy-latency' => [Object],
+      'x-kong-upstream-latency' => [Object],
+      'x-permitted-cross-domain-policies' => [Object],
+      'x-request-id' => [Object],
+      'x-xss-protection' => [Object]
+    },
+    [Symbol(headers map sorted)]: null
+  }
+}
