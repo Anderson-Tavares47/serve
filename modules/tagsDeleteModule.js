99bcbdf90ad7fa8925078f44afac6b4d2a61b3d5
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-05 23:12:02 -0300

    Add files via upload

---
+++ modules/tagsDeleteModule.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.delete('/tags/:tag', async (req, res) => {
+    const tagName = req.params.tag;
+
+    try {
+        const result = await db.query('DELETE FROM tags WHERE tag = $1 RETURNING *', [tagName]);
+
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]);
+        } else {
+            res.status(404).send('Tag não encontrada');
+        }
+    } catch (error) {
+        console.error('Erro ao excluir tag:', error);
+        res.status(500).send('Erro Interno do Servidor');
+    }
+});
+
+module.exports = router;
