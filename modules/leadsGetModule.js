1eee1513233e53039f19e7de6b70dbd165e6477a
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-05 22:50:57 -0300

    Add files via upload

---
+++ modules/leadsGetModule.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await db.query('SELECT * FROM leads');
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Erro ao obter leads:', error);
+        res.status(500).send('Erro Interno do Servidor');
+    }
+});
