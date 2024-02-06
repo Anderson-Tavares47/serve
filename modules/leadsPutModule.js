c8184ef4d7ed884f56f3c6fc8e7c7c8988560c0b
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-05 22:53:34 -0300

    Add files via upload

---
+++ modules/leadsPutModule.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+router.put('/:id', async (req, res) => {
+    const leadId = req.params.id;
+    const { nome, celular, email, tag } = req.body;
+
+    try {
+        const result = await db.query('UPDATE leads SET nome = $1, celular = $2, email = $3, tag = $4 WHERE id = $5 RETURNING *', [nome, celular, email, tag, leadId]);
+
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]);
+        } else {
+            res.status(404).send('Lead não encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao atualizar lead:', error);
+        res.status(500).send('Erro Interno do Servidor');
+    }
+});
