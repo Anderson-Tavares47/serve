db0539397eb8e13fd061dfa86cb5900942865c89
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-05 23:16:28 -0300

    Update tagsDeleteModule.js

--- modules/tagsDeleteModule.js
+++ modules/tagsDeleteModule.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:tag', async (req, res) => {
     const tagName = req.params.tag;
 
     try {
