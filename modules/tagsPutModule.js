3273bffa2947a2a83f994919e9410c6957a11936
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-13 23:01:56 -0300

    Update tagsPutModule.js

--- modules/tagsPutModule.js
+++ modules/tagsPutModule.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
+const validateApiKey = require('./validateApiKey');
 
-router.put("/:tagName", async (req, res) => {
+router.put("/:tagName", validateApiKey, async (req, res) => {
   const tagName = req.params.tagName;
   const { newTagName } = req.body;
 
