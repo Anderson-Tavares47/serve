2b2ac5d75ba08d4157b500a00f69363c7ce0af5d
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-13 22:22:06 -0300

    Update leadsGetModule.js

--- modules/leadsGetModule.js
+++ modules/leadsGetModule.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../db");
+const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
-router.get("/", validateApiKey, async (req, res) => {
-  try {
-    const result = await db.query("SELECT * FROM leads");
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Erro ao obter leads:", error);
-    res.status(500).send("Erro Interno do Servidor");
-  }
+router.get('/', validateApiKey, async (req, res) => {
+    try {
+        const leads = await db.query('SELECT * FROM leads');
+        res.status(200).json(leads.rows);
+    } catch (error) {
+        console.error('Erro ao recuperar leads:', error);
+        res.status(500).json({ error: 'Erro Interno do Servidor' });
+    }
 });
 
 module.exports = router;
