0789a5e67662354621c3b1869b986701b726fd34
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-13 22:57:50 -0300

    Update leadsDeleteModule.js

--- modules/leadsDeleteModule.js
+++ modules/leadsDeleteModule.js
@@ -7,10 +7,10 @@ router.delete('/:id', validateApiKey, async (req, res) => {
     const leadId = req.params.id;
 
     try {
-        const result = await db.query('DELETE FROM leads WHERE id = $1 RETURNING *', [leadId]);
+        const result = await db.query('DELETE FROM lead WHERE id = $1 RETURNING *', [leadId]);
 
-        if (result.rows.length > 0) {
-            res.json(result.rows[0]);
+        if (result) {
+            res.json(result);
         } else {
             res.status(404).send('Lead não encontrado');
         }
