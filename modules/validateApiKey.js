12e3918641dad7bd2224063a0c34b278c2e71941
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-13 22:30:47 -0300

    Update validateApiKey.js

--- modules/validateApiKey.js
+++ modules/validateApiKey.js
@@ -17,7 +17,7 @@ const validateApiKey = async (req, res, next) => {
     next();
   } catch (error) {
     console.error('Erro ao validar a API_KEY:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
+    return res.status(500).json({ error: 'Erro interno do servidor' });
   }
 };
 
