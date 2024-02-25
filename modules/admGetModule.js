a99d5b186ec96a1c0b74f07f53cbfd56ab6218c9
everton <cadonaenrike@gmail.com>
2024-02-25 11:09:52 -0300

    ajustei o admGet pra retornar se for adm por id

--- modules/admGetModule.js
+++ modules/admGetModule.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const db = require('../db');
+const express = require("express");
+const db = require("../db");
 const router = express.Router();
-const validateApiKey = require('./validateApiKey');
+const validateApiKey = require("./validateApiKey");
 
-
-router.get('/', validateApiKey, async (req, res) => {
+// Rota para buscar todos os usuários
+router.get("/", validateApiKey, async (req, res) => {
   try {
-    
-    const usuarios = await db.any('SELECT * FROM adms');
-
+    const usuarios = await db.any("SELECT * FROM adms");
     res.status(200).json(usuarios);
   } catch (error) {
-    console.error('Erro ao recuperar usuários:', error);
-    res.status(500).json({ error: 'Erro ao recuperar usuários' });
+    console.error("Erro ao recuperar usuários:", error);
+    res.status(500).json({ error: "Erro ao recuperar usuários" });
+  }
+});
+
+// Rota para buscar um usuário específico pelo ID
+router.get("/:id", validateApiKey, async (req, res) => {
+  const userId = parseInt(req.params.id); // Garante que o ID seja um número
+  if (!userId) {
+    return res.status(400).json({ error: "ID de usuário inválido" });
+  }
+
+  try {
+    const query = "SELECT * FROM adms WHERE id = $1";
+    const usuario = await db.oneOrNone(query, [userId]);
+
+    if (usuario) {
+      res.status(200).json(usuario);
+    } else {
+      res.status(404).json({ error: "Usuário não encontrado" });
+    }
+  } catch (error) {
+    console.error(`Erro ao recuperar o usuário com ID ${userId}:`, error);
+    res.status(500).json({ error: "Erro ao recuperar usuário" });
   }
 });
 
--- modules/loginModule.js
+++ modules/loginModule.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const db = require('../db');
-const validateApiKey = require('./validateApiKey');
+const db = require("../db");
+const validateApiKey = require("./validateApiKey");
 
-router.post('/', validateApiKey, async (req, res) => {
+router.post("/", validateApiKey, async (req, res) => {
   const { email, senha } = req.body;
 
   if (!email || !senha) {
-    return res.status(400).json({ error: 'E-mail e senha são obrigatórios' });
+    return res.status(400).json({ error: "E-mail e senha são obrigatórios" });
   }
 
   try {
-    const user = await db.oneOrNone('SELECT * FROM usuarios WHERE email = $1', email);
+    const user = await db.oneOrNone(
+      "SELECT * FROM usuarios WHERE email = $1",
+      email
+    );
 
-    if (user && await bcrypt.compare(senha, user.senha)) {
+    if (user && (await bcrypt.compare(senha, user.senha))) {
       const userData = {
         id: user.id,
         nome: user.nome,
@@ -22,15 +25,15 @@ router.post('/', validateApiKey, async (req, res) => {
       };
 
       res.status(200).json({
-        message: 'Login bem-sucedido',
+        message: "Login bem-sucedido",
         user: userData,
       });
     } else {
-      res.status(401).json({ error: 'Credenciais inválidas' });
+      res.status(401).json({ error: "Credenciais inválidas" });
     }
   } catch (error) {
-    console.error('Erro ao fazer login:', error);
-    res.status(500).json({ error: 'Erro ao fazer login' });
+    console.error("Erro ao fazer login:", error);
+    res.status(500).json({ error: "Erro ao fazer login" });
   }
 });
 
