9df73ba234ec8cb72efbc2d0c98b57f814951d87
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-25 23:57:33 -0300

    Add files via upload

---
+++ modules/subUserPutModule.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const validateApiKey = require('./validateApiKey');
+
+router.put('/:id', validateApiKey, async (req, res) => {
+  const { id } = req.params;
+  const { idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto } = req.body;
+
+  try {
+    console.log(`Atualizando subUser com ID ${id}`);
+    const result = await db.one('UPDATE subUser SET idAdmin = $1, nome = $2, sobrenome = $3, cpf = $4, celular = $5, cargo = $6, nivelAcesso = $7, email = $8, foto = $9 WHERE id = $10 RETURNING *', [idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto, id]);
+    res.json(result);
+  } catch (error) {
+    console.error('Erro ao atualizar o subUser:', error);
+    res.status(500).send('Erro Interno do Servidor');
+  }
+});
