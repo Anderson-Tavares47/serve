80c15f3742ce6658c8b6b0fca0d7cad69e7596fc
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-25 23:40:07 -0300

    Update subUserPostModule.js

--- modules/subUserPostModule.js
+++ modules/subUserPostModule.js
@@ -4,11 +4,11 @@ const db = require("../db");
 const validateApiKey = require('./validateApiKey');
 
 router.post('/', validateApiKey, async (req, res) => {
-  const { isAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, idIsAdmin } = req.body;
+  const { idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto } = req.body;
 
   try {
     console.log('Criando subUser:', nome);
-    const result = await db.one('INSERT INTO subUser (isAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, idIsAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *', [isAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, idIsAdmin]);
+    const result = await db.one('INSERT INTO subUser (idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *', [idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto]);
     res.json(result);
   } catch (error) {
     console.error('Erro ao criar um novo subUser:', error);
