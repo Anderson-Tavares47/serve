511d6d4bb53ad6271f0b916b1b6f828e7ee5f362
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 20:46:49 -0300

    Update usuariosPutModule.js

--- modules/usuariosPutModule.js
+++ modules/usuariosPutModule.js
@@ -12,14 +12,13 @@ router.put('/:id', validateApiKey, async (req, res) => {
       const columns = Object.keys(opcional).map((key, index) => `${key} = $${index + 7}`).join(', ');
       const values = Object.values(opcional);
       // Corrigindo a consulta SQL para usar "senha" em vez de "hashedsenha"
-      const query = `UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, senha = $4, isAdmin = $5, cargo = $6, ${columns} WHERE id = $${values.length + 6} RETURNING *`;
+      const query = `UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, senha = $4, isAdmin = $5, cargo = $6, ${columns} WHERE id = $${Object.keys(opcional).length + 6} RETURNING *`;
       const result = await db.one(query, [nome, sobrenome, email, senha, isAdmin, cargo, ...values, id]);
       res.json(result);
     } catch (error) {
       console.error('Erro ao atualizar usuário por ID:', error);
       res.status(500).send('Erro Interno do Servidor');
     }
-  });
+});
 
-
-  module.exports = router;
+module.exports = router;
