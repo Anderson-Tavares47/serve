02aa0e650423c1f97f8574bb317d342d5c5bcc0c
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 00:05:25 -0300

    Update planosPutModule.js

--- modules/planosPutModule.js
+++ modules/planosPutModule.js
@@ -6,6 +6,7 @@ const validateApiKey = require('./validateApiKey');
 router.put('/:id', validateApiKey, async (req, res) => {
     const { id } = req.params;
     const { nome, valor, descricao } = req.body;
+    console.log(id);
 
     try {
         const result = await db.query('UPDATE planos SET nome = $1, valor = $2, descricao = $3 WHERE id = $4 RETURNING *', [nome, valor, descricao, id]);
