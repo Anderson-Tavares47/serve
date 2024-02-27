a74751e84e91f426d26dcab607613cbf730f05f8
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 00:12:37 -0300

    Update server.js
    wreedas

--- server.js
+++ server.js
@@ -106,6 +106,7 @@ app.use("/getPlans", planosGet);
 // app.use("/deletePlan", planosDelete);
 // app.use("/getPlansId", planosGetIdModule);
 
+
 app.listen(port, () => {
   console.log(`Servidor está ouvindo em http://localhost:${port}`);
 });
