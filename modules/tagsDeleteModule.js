19385130ab1875a08fcbec7f6732064617bbd000
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-26 22:40:09 -0300

    Update tagsDeleteModule.js

--- modules/tagsDeleteModule.js
+++ modules/tagsDeleteModule.js
@@ -13,7 +13,7 @@ router.delete("/:tagName", validateApiKey, async (req, res) => {
     );
 
     if (result.rows.length > 0) {
-      res.json(result.rows[0]);
+      res.json(result);
     } else {
       res.status(404).send("Tag não encontrada");
     }
