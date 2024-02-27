05c98399d217d15dabc17279f0020e1bd8d1cafe
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 19:18:14 -0300

    Update usuariosPutModule.js

--- modules/usuariosPutModule.js
+++ modules/usuariosPutModule.js
@@ -5,14 +5,14 @@ const validateApiKey = require('./validateApiKey');
 
 router.put('/:id', validateApiKey, async (req, res) => {
     const { id } = req.params;
-    const { nome, sobrenome, email, hashedSenha, isAdmin, ...opcional } = req.body;
+    const { nome, sobrenome, email, senha, isAdmin, ...opcional } = req.body;
   
     try {
       console.log(`Atualizando usuário com ID ${id}`);
       const columns = Object.keys(opcional).map((key, index) => `${key} = $${index + 6}`).join(', ');
       const values = Object.values(opcional);
       const query = `UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, hashedsenha = $4, isAdmin = $5, ${columns} WHERE id = $${values.length + 6} RETURNING *`;
-      const result = await db.one(query, [nome, sobrenome, email, hashedsenha, isAdmin, ...values, id]);
+      const result = await db.one(query, [nome, sobrenome, email, senha, isAdmin, ...values, id]);
       res.json(result);
     } catch (error) {
       console.error('Erro ao atualizar usuário por ID:', error);
@@ -21,3 +21,4 @@ router.put('/:id', validateApiKey, async (req, res) => {
   });
   
   module.exports = router;
+
