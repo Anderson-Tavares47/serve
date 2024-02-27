af3c5cce323068eba785b6d73f991dafddf878ee
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 20:26:29 -0300

    Update planosGetModule.js

--- modules/planosGetModule.js
+++ modules/planosGetModule.js
@@ -3,17 +3,12 @@ const router = express.Router();
 const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
-router.get('/:id', validateApiKey, async (req, res) => {
-    const { id } = req.params;
-
+router.get('/', validateApiKey, async (req, res) => {
     try {
-        const result = await db.query('SELECT * FROM planos WHERE id = $1', [id]);
-        if (result.rows.length === 0) {
-            return res.status(404).json({ message: 'Plano não encontrado' });
-        }
-        res.json(result.rows[0]);
+        const result = await db.query('SELECT * FROM planos');
+        res.json(result.rows);
     } catch (error) {
-        console.error('Erro ao obter informações do plano:', error);
+        console.error('Erro ao obter informações dos planos:', error);
         res.status(500).send('Erro Interno do Servidor');
     }
 });
