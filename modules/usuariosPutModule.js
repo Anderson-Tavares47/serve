b65fdef0d6c9106889d61aadbab65a97655ad8d0
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 19:40:05 -0300

    Update usuariosPutModule.js

--- modules/usuariosPutModule.js
+++ modules/usuariosPutModule.js
@@ -11,7 +11,8 @@ router.put('/:id', validateApiKey, async (req, res) => {
       console.log(`Atualizando usuário com ID ${id}`);
       const columns = Object.keys(opcional).map((key, index) => `${key} = $${index + 6}`).join(', ');
       const values = Object.values(opcional);
-      const query = `UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, hashedsenha = $4, isAdmin = $5, ${columns} WHERE id = $${values.length + 6} RETURNING *`;
+      // Corrigindo a consulta SQL para usar "senha" em vez de "hashedsenha"
+      const query = `UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, senha = $4, isAdmin = $5, ${columns} WHERE id = $${values.length + 6} RETURNING *`;
       const result = await db.one(query, [nome, sobrenome, email, senha, isAdmin, ...values, id]);
       res.json(result);
     } catch (error) {
@@ -21,4 +22,3 @@ router.put('/:id', validateApiKey, async (req, res) => {
   });
   
   module.exports = router;
-
