823e8c2d2f5833a9a824e3e1bac8122df4ca0e64
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-26 22:38:09 -0300

    Update tagsGetModule.js

--- modules/tagsGetModule.js
+++ modules/tagsGetModule.js
@@ -7,7 +7,7 @@ const validateApiKey = require('./validateApiKey');
 router.get("/", validateApiKey, async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM tags");
-    res.json(result.rows);
+    res.json(result);
   } catch (error) {
     console.error("Erro ao obter tags:", error);
     res.status(500).send("Erro Interno do Servidor");
