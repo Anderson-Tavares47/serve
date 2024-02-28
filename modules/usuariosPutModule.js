149891d68e5706e55eb2711e00fbc25eaec893a7
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 21:19:35 -0300

    Update usuariosPutModule.js

--- modules/usuariosPutModule.js
+++ modules/usuariosPutModule.js
@@ -3,17 +3,34 @@ const router = express.Router();
 const db = require("../db");
 const validateApiKey = require('./validateApiKey');
 
-// Rota para obter todos os usuários
-router.get('/', validateApiKey, async (req, res) => {
+router.put('/:id', validateApiKey, async (req, res) => {
+  const { id } = req.params; 
+  const userData = req.body;
+
   try {
-    console.log("Obtendo todos os usuários...");
-    const users = await db.any('SELECT * FROM usuarios');
-    res.json(users);
+    console.log(`Atualizando usuário com ID ${id}`);
+    
+    let query = 'UPDATE usuarios SET ';
+    const values = [];
+    let index = 1;
+    
+    for (const key in userData) {
+      query += `${key} = $${index}, `;
+      values.push(userData[key]);
+      index++;
+    }
+    
+    query = query.slice(0, -2);
+    
+    query += ` WHERE id = $${index} RETURNING *`;
+    values.push(id);
+    
+    const result = await db.one(query, values);
+    res.json(result);
   } catch (error) {
-    console.error('Erro ao obter usuários:', error);
+    console.error('Erro ao atualizar usuário:', error);
     res.status(500).send('Erro Interno do Servidor');
   }
 });
 
 module.exports = router;
-
