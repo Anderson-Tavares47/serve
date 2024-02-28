7284d797e186d63017e2fc8c0d509f7ffc5cbfd5
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 21:56:49 -0300

    Update planoPostModule.js

--- modules/planoPostModule.js
+++ modules/planoPostModule.js
@@ -6,12 +6,16 @@ const validateApiKey = require('./validateApiKey');
 
 router.post('/', validateApiKey, async (req, res) => {
     const { nome, valor, descricao } = req.body;
-    console.log(nome, valor, descricao)
+    console.log(nome, valor, descricao);
 
     try {
         const result = await db.query('INSERT INTO planos (nome, valor, descricao) VALUES ($1, $2, $3) RETURNING *', [nome, valor, descricao]);
         
-        res.status(201).json(result.rows[0]);
+        if (result) {
+            res.status(201).json(result);
+        } else {
+            res.status(404).send('Nenhum plano foi criado.');
+        }
     } catch (error) {
         console.error('Erro ao criar um novo plano:', error);
         res.status(500).send('Erro Interno do Servidor');
