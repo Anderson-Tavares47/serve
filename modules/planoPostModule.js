0d55a5852215376e1fbef0746c61c9f63fd97e43
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 21:54:16 -0300

    Update planoPostModule.js

--- modules/planoPostModule.js
+++ modules/planoPostModule.js
@@ -6,7 +6,7 @@ const validateApiKey = require('./validateApiKey');
 
 router.post('/', validateApiKey, async (req, res) => {
     const { nome, valor, descricao } = req.body;
-    console.log(nome, valor, decricao)
+    console.log(nome, valor, descricao)
 
     try {
         const result = await db.query('INSERT INTO planos (nome, valor, descricao) VALUES ($1, $2, $3) RETURNING *', [nome, valor, descricao]);
