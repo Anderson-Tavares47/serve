d5d7f10cacc584c6a040443a065a631ffd0e997a
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 21:05:39 -0300

    Update usuariosPutModule.js

--- modules/usuariosPutModule.js
+++ modules/usuariosPutModule.js
@@ -9,11 +9,25 @@ router.put('/:id', validateApiKey, async (req, res) => {
   
     try {
       console.log(`Atualizando usuário com ID ${id}`);
+      
+      // Construindo a parte da consulta SQL para os campos opcionais
       const columns = Object.keys(opcional).map((key, index) => `${key} = $${index + 7}`).join(', ');
+      
+      // Construindo os valores dos parâmetros da consulta SQL
       const values = Object.values(opcional);
+      
       // Corrigindo a consulta SQL para usar "senha" em vez de "hashedsenha"
-      const query = `UPDATE usuarios SET nome = $1, sobrenome = $2, email = $3, senha = $4, isAdmin = $5, cargo = $6, ${columns} WHERE id = $${Object.keys(opcional).length + 6} RETURNING *`;
+      const query = `
+        UPDATE usuarios 
+        SET nome = $1, sobrenome = $2, email = $3, senha = $4, isAdmin = $5, cargo = $6, ${columns} 
+        WHERE id = $${Object.keys(opcional).length + 6} 
+        RETURNING *
+      `;
+      
+      // Executando a consulta no banco de dados
       const result = await db.one(query, [nome, sobrenome, email, senha, isAdmin, cargo, ...values, id]);
+      
+      // Retornando o resultado da consulta como resposta
       res.json(result);
     } catch (error) {
       console.error('Erro ao atualizar usuário por ID:', error);
