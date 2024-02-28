4d03192421394809ff78a4ef0ca1104b7109c6b3
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-27 23:39:58 -0300

    Update tokenGetModule.js

--- modules/tokenGetModule.js
+++ modules/tokenGetModule.js
@@ -9,8 +9,8 @@ router.get('/:id', validateApiKey, async (req, res) => {
     try {
         const result = await db.query('SELECT * FROM tokens WHERE id = $1', [id]);
         
-        if (result.rows.length > 0) {
-            res.json(result.rows[0]);
+        if (result) {
+            res.json(result);
         } else {
             res.status(404).send('Token não encontrado.');
         }
