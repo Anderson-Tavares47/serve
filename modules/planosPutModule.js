d1be684895a154246cbcee2f2532f4c6316d5469
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 17:08:29 -0300

    Update planosPutModule.js

--- modules/planosPutModule.js
+++ modules/planosPutModule.js
@@ -5,15 +5,16 @@ const validateApiKey = require('./validateApiKey');
 
 router.put('/:id', validateApiKey, async (req, res) => {
     const { id } = req.params;
-    const { nome, valor, descricao } = req.body;
+    const { nome, valor, descricao, userid } = req.body;
     console.log(id);
 
     try {
-        const result = await db.query('UPDATE planos SET nome = $1, valor = $2, descricao = $3 WHERE id = $4 RETURNING *', [nome, valor, descricao, id]);
-        if (result.rows) {
+        const result = await db.query('UPDATE planos SET nome = $1, valor = $2, descricao = $3, userid = $4 WHERE id = $5 RETURNING *', [nome, valor, descricao, userid, id]);
+
+        if (result.length === 0)
             return res.status(404).json({ message: 'Plano não encontrado' });
         }
-        res.json(result);
+        res.json(result.rows[0]);
     } catch (error) {
         console.error('Erro ao atualizar informações do plano:', error);
         res.status(500).send('Erro Interno do Servidor');
