fc731cfd10513b7db7688546a343e3ac9f74f97b
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 18:19:49 -0300

    Create campanhasLeadsGetModule.js

---
+++ modules/campanhasLeadsGetModule.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const db = require('../db');
+const router = express.Router();
+const validateApiKey = require('./validateApiKey');
+
+router.get('/total', validateApiKey, async (req, res) => {
+  try {
+    const resultadoCampanhas = await db.one('SELECT SUM(valor) AS valor_total_campanhas FROM campanhas');
+
+    const resultadoLead = await db.one('SELECT SUM(valor) AS valor_total_lead FROM lead');
+
+    const resultadoTotal = {
+      valor_total_campanhas: resultadoCampanhas.valor_total_campanhas,
+      valor_total_lead: resultadoLead.valor_total_lead
+    };
+
+    res.status(200).json(resultadoTotal);
+  } catch (error) {
+    console.error('Erro ao calcular o valor total das campanhas e leads:', error);
+    res.status(500).json({ error: 'Erro ao calcular o valor total das campanhas e leads' });
+  }
+});
+
+module.exports = router;
