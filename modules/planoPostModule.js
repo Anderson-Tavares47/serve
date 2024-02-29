c4411e71674285cc82623f9513b37e205c97abf2
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 14:26:45 -0300

    Update planoPostModule.js

--- modules/planoPostModule.js
+++ modules/planoPostModule.js
@@ -5,14 +5,13 @@ const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
 router.post('/', validateApiKey, async (req, res) => {
-    const { nome, valor, descricao } = req.body;
-    console.log(nome, valor, descricao);
+    const { nome, valor, descricao, userid } = req.body; // Adicionando userid ao destructuring dos dados do corpo da requisição
 
     try {
-        const result = await db.query('INSERT INTO planos (nome, valor, descricao) VALUES ($1, $2, $3) RETURNING *', [nome, valor, descricao]);
-        
-        if (result) {
-            res.status(201).json(result);
+        const result = await db.query('INSERT INTO planos (nome, valor, descricao, userid) VALUES ($1, $2, $3, $4) RETURNING *', [nome, valor, descricao, userid]);
+
+        if (result.rows.length > 0) { // Verificando se a inserção foi bem-sucedida
+            res.status(201).json(result.rows[0]);
         } else {
             res.status(404).send('Nenhum plano foi criado.');
         }
