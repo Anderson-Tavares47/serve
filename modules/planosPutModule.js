dc3cce083ee22951c4930fd8af6d1f15758ba0ef
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 17:14:53 -0300

    Update planosPutModule.js

--- modules/planosPutModule.js
+++ modules/planosPutModule.js
@@ -5,13 +5,13 @@ const validateApiKey = require('./validateApiKey');
 
 router.put('/:id', validateApiKey, async (req, res) => {
     const { id } = req.params;
-    const { nome, valor, descricao, userid } = req.body;
+    const { nome, valor, descricao, userid } = req.body; // Adicionando userid ao destructuring dos dados do corpo da requisição
     console.log(id);
 
     try {
         const result = await db.query('UPDATE planos SET nome = $1, valor = $2, descricao = $3, userid = $4 WHERE id = $5 RETURNING *', [nome, valor, descricao, userid, id]);
 
-        if (result.length === 0)
+        if (result.length === 0) {
             return res.status(404).json({ message: 'Plano não encontrado' });
         }
         res.json(result.rows[0]);
