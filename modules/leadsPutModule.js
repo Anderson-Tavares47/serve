a44c51c3813bac3c2e6dbe99a3859e185689e4bc
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 21:44:38 -0300

    Update leadsPutModule.js

--- modules/leadsPutModule.js
+++ modules/leadsPutModule.js
@@ -5,12 +5,12 @@ const validateApiKey = require('./validateApiKey');
 
 router.put('/:id', validateApiKey, async (req, res) => {
     const leadId = req.params.id;
-    const { nome, celular, email, tag } = req.body;
+    const { nome, celular, email, tag, userid } = req.body;
 
     try {
-        const result = await db.query('UPDATE lead SET nome = $1, celular = $2, email = $3, tag = $4 WHERE id = $5 RETURNING *', [nome, celular, email, tag, leadId]);
+        const result = await db.query('UPDATE lead SET nome = $1, celular = $2, email = $3, tag = $4, userid = $5 WHERE id = $6 RETURNING *', [nome, celular, email, tag, userid, leadId]);
 
-        if (result) {
+        if (result.length) {
             res.json(result);
         } else {
             res.status(404).send('Lead não encontrado');
