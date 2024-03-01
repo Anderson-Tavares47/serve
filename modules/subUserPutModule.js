58d9256747e88895829b14d707f8ef8d37b4d6ec
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-03-01 00:37:32 -0300

    Update subUserPutModule.js

--- modules/subUserPutModule.js
+++ modules/subUserPutModule.js
@@ -10,7 +10,7 @@ router.put('/:id', validateApiKey, async (req, res) => {
   
   try {
     console.log(`Atualizando subUser com ID ${id}`);
-    const result = await db.one('UPDATE subUser SET idAdmin = $1, nome = $2, sobrenome = $3, cpf = $4, celular = $5, cargo = $6, nivelAcesso = $7, email = $8, foto = $9 WHERE id = $10 RETURNING *', [idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto, id]);
+    const result = await db.one('UPDATE subUser SET idAdmin = $1, nome = $2, sobrenome = $3, cpf = $4, celular = $5, cargo = $6, "nivelAcesso" = $7, email = $8, foto = $9 WHERE id = $10 RETURNING *', [idAdmin, nome, sobrenome, cpf, celular, cargo, nivelAcesso, email, foto, id]);
     res.json(result);
   } catch (error) {
     console.error('Erro ao atualizar o subUser:', error);
