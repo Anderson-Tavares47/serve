a4590e468f5c7dda4cd4a1a3f774c0b9e703bed2
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-03-01 01:53:37 -0300

    Update campanhasLeadsGetModule.js

--- modules/campanhasLeadsGetModule.js
+++ modules/campanhasLeadsGetModule.js
@@ -4,22 +4,26 @@ const router = express.Router();
 const validateApiKey = require('./validateApiKey');
 
 router.get('/', validateApiKey, async (req, res) => {
+  const { userid } = req.query; // Obtenha o userid dos parâmetros de consulta
+  
   try {
-    const resultadoCampanhas = await db.one('SELECT SUM(valor) AS valor_total_campanhas FROM campanhas');
+    // Consulta SQL para obter o total de itens na tabela de campanhas para o userid fornecido
+    const resultadoCampanhas = await db.one('SELECT COUNT(*) AS total_campanhas FROM campanhas WHERE userid = $1', [userid]);
 
-    const resultadoLead = await db.one('SELECT SUM(valor) AS valor_total_lead FROM lead');
+    // Consulta SQL para obter o total de itens na tabela de leads para o userid fornecido
+    const resultadoLead = await db.one('SELECT COUNT(*) AS total_lead FROM lead WHERE userid = $1', [userid]);
 
+    // Construir o resultado total
     const resultadoTotal = {
-      valor_total_campanhas: resultadoCampanhas.valor_total_campanhas,
-      valor_total_lead: resultadoLead.valor_total_lead
+      total_campanhas: resultadoCampanhas.total_campanhas,
+      total_lead: resultadoLead.total_lead
     };
 
     res.status(200).json(resultadoTotal);
   } catch (error) {
-    console.error('Erro ao calcular o valor total das campanhas e leads:', error);
-    res.status(500).json({ error: 'Erro ao calcular o valor total das campanhas e leads' });
+    console.error('Erro ao calcular o total de itens das campanhas e leads:', error);
+    res.status(500).json({ error: 'Erro ao calcular o total de itens das campanhas e leads' });
   }
 });
 
 module.exports = router;
-
