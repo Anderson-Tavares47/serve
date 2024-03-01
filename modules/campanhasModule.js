8c3edc7126083584be064c57542ea3e4dfdf6bb1
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 22:32:58 -0300

    Update campanhasModule.js

--- modules/campanhasModule.js
+++ modules/campanhasModule.js
@@ -5,14 +5,14 @@ const validateApiKey = require('./validateApiKey');
 
 router.post('/pastas/:id', validateApiKey, async (req, res) => {
   const { id } = req.params;
-  const { name } = req.body;
+  const { name, userid } = req.body;
 
   try {
-    const result = await db.one('INSERT INTO campanhas (name, pasta_id) VALUES ($1, $2) RETURNING *', [name, id]);
+    const result = await db.one('INSERT INTO campanhas (name, pasta_id, userid) VALUES ($1, $2, $3) RETURNING *', [name, id, userid]);
     res.json(result);
   } catch (error) {
-    console.error('Error creating a new campaign:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Erro ao criar uma nova campanha:', error);
+    res.status(500).send('Erro Interno do Servidor');
   }
 });
 
