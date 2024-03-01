6dd15969ec105c765a97b1ffd82df26326819f7d
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-02-29 21:34:57 -0300

    Update leadsPostModule.js

--- modules/leadsPostModule.js
+++ modules/leadsPostModule.js
@@ -4,14 +4,14 @@ const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
 router.post('/', validateApiKey, async (req, res) => {
-    const { nome, celular, email, tag } = req.body;
+    const { nome, celular, email, tag, userid } = req.body;
 
     try {
-        const result = await db.query('INSERT INTO lead (nome, celular, email, tag) VALUES ($1, $2, $3, $4) RETURNING *', [nome, celular, email, tag]);
+        const result = await db.query('INSERT INTO lead (nome, celular, email, tag, userid) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nome, celular, email, tag, userid]);
 
         console.log(result);
 
-        if (result) {
+        if (result.length) {
             res.status(201).json(result);
         } else {
             res.status(500).send('Erro Interno do Servidor: Nenhum resultado retornado após a inserção.');
