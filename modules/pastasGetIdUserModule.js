fe86e0b9f49e3c1c42ec1261bb280ec7de0d45c5
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-03-07 21:58:09 -0300

    Update pastasGetIdUserModule.js

--- modules/pastasGetIdUserModule.js
+++ modules/pastasGetIdUserModule.js
@@ -7,9 +7,9 @@ router.get('/:userid', validateApiKey, async (req, res) => {
   const { userid } = req.params;
 
   try {
-    const usuario = await db.one('SELECT * FROM pastas WHERE userid = $1', userid);
+    const pastas = await db.any('SELECT * FROM pastas WHERE userid = $1', userid);
 
-    res.status(200).json(usuario);
+    res.status(200).json(pastas);
   } catch (error) {
     console.error('Erro ao recuperar o usuário por userid:', error);
     res.status(500).json({ error: 'Erro ao recuperar o usuário por userid' });
@@ -17,3 +17,4 @@ router.get('/:userid', validateApiKey, async (req, res) => {
 });
 
 module.exports = router;
+
