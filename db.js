12ce5db33f6b3bb0c2bbf45ebf1e42e89e4af7c8
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-03-07 22:44:22 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
-// database.js
 const pgp = require('pg-promise')();
 
-// Substitua as informações do banco de dados com as fornecidas pelo ElephantSQL
-const db = pgp('postgres://default:tc69oSAOvPFH@ep-red-wildflower-a4b5n1jo.us-east-1.aws.neon.tech:5432/verceldb');
-
+const db = pgp({
+  connectionString: 'postgres://default:tc69oSAOvPFH@ep-red-wildflower-a4b5n1jo.us-east-1.aws.neon.tech:5432/verceldb',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = db;
