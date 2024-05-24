fcad77db69a01cc710409ad696c0fb368c9ca59a
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-05-24 00:40:38 -0300

    Update whatsappFluxGetId.js

--- modules/whatsappFluxGetId.js
+++ modules/whatsappFluxGetId.js
@@ -4,16 +4,17 @@ const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
 router.get('/:id', validateApiKey, async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      const result = await db.one('SELECT * FROM whatsapp_flux WHERE id = $1', [id]);
-      res.json(result);
-    } catch (error) {
-      console.error('Erro ao obter registro de whatsapp_flux:', error);
-      res.status(500).send('Erro Interno do Servidor');
-    }
-  });
-  
-  module.exports = router;
+  const { id } = req.params;
+
+  try {
+    const result = await db.one('SELECT * FROM whatsapp_flux WHERE id = $1', [id]);
+    res.json(result);
+  } catch (error) {
+    console.error('Erro ao obter registro de whatsapp_flux:', error);
+    res.status(500).send('Erro Interno do Servidor');
+  }
+});
+
+module.exports = router;
+
   
