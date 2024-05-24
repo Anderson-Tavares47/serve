917df914a9674d31b891ca28738d3d434a95ba09
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-05-24 00:40:49 -0300

    Update whatsappFluxPut.js

--- modules/whatsappFluxPut.js
+++ modules/whatsappFluxPut.js
@@ -4,17 +4,19 @@ const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
 router.put('/:id', validateApiKey, async (req, res) => {
-    const { id } = req.params;
-    const { name, id_pasta, idUser, content } = req.body;
-  
-    try {
-      const result = await db.one('UPDATE whatsapp_flux SET name = $1, id_pasta = $2, idUser = $3, content = $4 WHERE id = $5 RETURNING *', [name, id_pasta, idUser, content, id]);
-      res.json(result);
-    } catch (error) {
-      console.error('Erro ao atualizar registro de whatsapp_flux:', error);
-      res.status(500).send('Erro Interno do Servidor');
-    }
-  });
-  
-  module.exports = router;
+  const { id } = req.params;
+  const { idUser, id_pasta, name, nodes, edges } = req.body;
+  const content = { idUser, id_pasta, name, nodes, edges };
+
+  try {
+    const result = await db.one('UPDATE whatsapp_flux SET content = $1 WHERE id = $2 RETURNING *', [content, id]);
+    res.json(result);
+  } catch (error) {
+    console.error('Erro ao atualizar registro de whatsapp_flux:', error);
+    res.status(500).send('Erro Interno do Servidor');
+  }
+});
+
+module.exports = router;
+
   
