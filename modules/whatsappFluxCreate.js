924fc33d651c766c4b34a804337bb34188be9c38
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-05-24 00:40:10 -0300

    Update whatsappFluxCreate.js

--- modules/whatsappFluxCreate.js
+++ modules/whatsappFluxCreate.js
@@ -4,14 +4,15 @@ const db = require('../db');
 const validateApiKey = require('./validateApiKey');
 
 router.post('/', validateApiKey, async (req, res) => {
-  const { name, id_pasta, content } = req.body;
+  const { idUser, id_pasta, name, nodes, edges } = req.body;
+  const content = { idUser, id_pasta, name, nodes, edges };
 
   try {
-    console.log('Criando flux:', name);
-    const result = await db.one('INSERT INTO whatsapp_flux (name, id_pasta, content) VALUES ($1, $2, $3) RETURNING *', [name, id_pasta, content]);
+    console.log('Criando whatsapp_flux:', name);
+    const result = await db.one('INSERT INTO whatsapp_flux (content) VALUES ($1) RETURNING *', [content]);
     res.json(result);
   } catch (error) {
-    console.error('Erro ao criar um novo flux:', error);
+    console.error('Erro ao criar um novo whatsapp_flux:', error);
     res.status(500).send('Erro Interno do Servidor');
   }
 });
